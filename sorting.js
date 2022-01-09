56356128ed36b8bcf47555caf92b5ee4fa0f21da
Rahul Kumar <87421972+fitrahul@users.noreply.github.com>
2022-01-09 11:34:45 +0530

    quick sort update

--- sorting.js
+++ sorting.js
@@ -1,4 +1,5 @@
 // 1. Bubble sort => 
+
 /*
 function runProgram(input) {
     var arr = input.trim().split('\n');
@@ -36,7 +37,8 @@ if (process.env.USERNAME === `hp`) {
 }
 */
 
-// *****************************************
+// *********************************************************
+// *********************************************************
 
 // 2. selection sort => 
 
@@ -79,42 +81,108 @@ if (process.env.USERNAME === `hp`) {
 }
 */
 
-// 3. quick sort =>
+// *********************************************************
+// *********************************************************
 
-function quickSort(A, left, right) {
+// 3. QUICK SORT =>
+
+/*
+function partition(items, left, right) {
+    var pivot = items[Math.floor((right + left) / 2)], //middle element
+        i = left, //left pointer
+        j = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            // swap(items, i, j); //swap two elements
+            [items[i], items[j]] = [items[j], items[i]]
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    let index;
     if (arr.length > 1) {
-        var 
+        index = partition(arr, left, right);
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        };
+        if (index < right) {
+            quickSort(arr, index, right);
+        };
     }
+    return arr;
 }
 
-function runProgram(input) {
-    var arr = input.trim().split('\n');
-    var N = +arr[0];
-    var A = arr[1].trim().split(" ").map(Number);
-    if (N === 1) console.log(A[0]);
-    else {
-        var cll = quickSort(A, left = 0, right = N-1);
-        console.log(cll);
+
+var items = [3, 5, 0, 9, 8];
+
+let sortedArray = quickSort(items);
+console.log(sortedArray);
+*/
+
+// *********************************************************
+// *********************************************************
+
+// <<<<<============  explanation of quicksort ===========>>>>>>
+
+function partition(items, left, right) {
+    console.log("items: " ,items);
+    var pivot = items[Math.floor((right + left) / 2)], //middle element
+        i = left, //left pointer
+        j = right; //right pointer
+    while (i <= j) {
+        console.log("i: ",i," j: ",j);
+        while (items[i] < pivot) {
+            console.log("item[i] :",items[i]," pivot: ",pivot);
+            console.log("i-: ",i);
+            i++;
+            console.log("i+: ",i);
+        }
+        while (items[j] > pivot) {
+            console.log("item[j] :",items[j]," pivot: ",pivot);
+            console.log("j-: ",j);
+            j--;
+            console.log("j+: ",j);
+        }
+        if (i <= j) {
+            // swap(items, i, j); //swap two elements
+            console.log("swap1 :",items[i]," ", items[j]);
+            [items[i], items[j]] = [items[j], items[i]]
+            console.log("swap2 :",items[i]," ", items[j]);
+            i++;
+            j--;
+        }
     }
+    console.log("position of pivot: ", i);
+    return i;
 }
-if (process.env.USERNAME === `hp`) {
-    runProgram(`5
-    3 5 0 9 8`);
-} else {
-    process.stdin.resume();
-    process.stdin.setEncoding(`ascii`);
-    let read = ``;
-    process.stdin.on(`data`, function (input) {
-        read += input;
-    });
-    process.stdin.on(`end`, function () {
-        read = read.replace(/\n$/, ``);
-        read = read.replace(/\n$/, ``);
-        runProgram(read);
-    });
-    process.on(`SIGINT`, function () {
-        read = read.replace(/\n$/, ``);
-        runProgram(read);
-        process.exit(0);
-    });
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    let index;
+    if (arr.length > 1) {
+        index = partition(arr, left, right);
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        };
+        if (index < right) {
+            quickSort(arr, index, right);
+        };
+    }
+    return arr;
 }
+
+
+var items = [3, 5, 0, 9, 8];
+// var items = [10,16,8,12,15,6,3,9,5];
+
+let sortedArray = quickSort(items);
+console.log(sortedArray);
